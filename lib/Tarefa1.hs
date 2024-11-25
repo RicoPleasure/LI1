405a8e3809f87cd690b82608cd85029fe75ed913
RicoPleasure <esprazj.dev@gmail.com>
2024-11-25 23:08:31 +0000

    feat: add some tarefa1 functions

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -11,16 +11,47 @@ module Tarefa1 where
 
 import LI12425
 
-validaJogo :: Jogo -> Bool
-validaJogo Jogo { 
+{-| Função que recebe todas as validações (validaMapa, validaBase) -}
+validaJogo :: Jogo -- Dados do jogo
+           -> Bool
+validaJogo (Jogo { 
               mapaJogo = mapa,
-              baseJogo = Base { posicaoBase = (baseX,baseY)},
-              portaisJogo = [ Portal {posicaoPortal = (x,y)} ] } =
-                undefined
+              baseJogo = Base { posicaoBase = (baseX,baseY),
+                                creditosBase = creditos},
+              portaisJogo = [ Portal {posicaoPortal = coordsPortal} ] }) =
+                   validaMapa mapa
+                && validaBase mapa (baseX,baseY)
 
--- A função que eu estava a falar era esta: deve funcionar mas não consegui testar 
-validaPosicaoObjeto :: (Float, Float) -> Jogo -> Bool
-validaPosicaoObjeto (x,y) (Jogo {mapaJogo = mapa}) = 
+{-| Valida a posição de um objeto qualquer dentro do mapa (base, portais, inimigos) -}
+validaPosicaoObjeto :: [[Terreno]] -- Mapa
+                    -> (Float, Float) -- Posição da base
+                    -> Bool
+validaPosicaoObjeto mapa (x,y) =
   x >= 0 && x < fromIntegral (length (head mapa)) && y >= 0 && y < fromIntegral (length mapa)
 
-{- existeBase :: Jogo ->  -}
+{-| Valida se o mapa está bem construído (isto é, o número de linhas é igual ao número de colunas) -}
+validaMapa :: [[Terreno]] -- Mapa
+           -> Bool
+validaMapa mapa = 
+  length (head mapa) == length mapa
+
+{-| Valida se a base está num terreno de "Terra" -}
+validaBase :: [[Terreno]] -- Mapa
+           -> (Float, Float) -- Posição da base
+           -> Bool
+validaBase mapa (indexX, indexY) =
+  verificaTerrenoBase mapa (indexX, indexY) == Terra 
+  && validaPosicaoObjeto mapa (indexX, indexY)
+
+{-| Pega o terreno da posição da base no mapa -}
+verificaTerrenoBase :: [[Terreno]] -- Mapa
+                -> (Float, Float) -- Posição da base
+                -> Terreno -- Terreno da base
+verificaTerrenoBase mapa (indexX,indexY) = terreno
+  where 
+    linhas = mapa !! (floor indexY)
+    terreno = linhas !! (floor indexX)
+
+-- TODO verificaCreditosBase :: 
+
+-- TODO verificaSobreposicao ::                 
