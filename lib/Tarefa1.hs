db06c97a6319bd38103f6bc5fdaed20c4a28c57d
Leandro Filipe Loureço Carvalho <a112021@uminho.pt>
2024-12-05 23:08:49 +0000

    Implementation of the Portals validation

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,17 +10,55 @@ Módulo para a realização da Tarefa 1 de LI1 em 2024/25.
 module Tarefa1 where
 
 import LI12425
+import Utils.UtilitariosPortal
 
+{-|
+  A função 'validaJogo' valida, recorrendo a múltiplas validações feitas por funções auxiliares, se um dado estado de Jogo é valido
+
+  ==__Objetos validados__
+  * 'Portal': recorrendo á função 'validaPortais'. 
+  * 'Base': recorrendo á função TODO:. 
+  * 'Torre': recorrendo á função TODO:.
+  * 'Mapa': recorrendo á função TODO:.
+
+  Para além da validação de cada tipo de objetos presente no 'Jogo', a função 'validaJogo' também valida se existem sobreposições entre os vários objetos, recorrendo á função 'verificaSobreposicoes'
+
+  ==__Exemplos de utilização__
+  >>> validaJogo Jogo {mapaJogo = [], baseJogo = Base {posicaoBase = (2,0)}, portaisJogo = [Portal {posicaoPortal = (0,0)}], torresJogo = []}
+  False
+  >>> validaJogo Jogo {mapaJogo = [[Terra, Terra, Terra, Relva]], baseJogo = Base {posicaoBase = (2,0)}, portaisJogo = [], torresJogo = []}
+  False
+  >>> validaJogo Jogo {mapaJogo = [[Terra, Terra, Terra, Relva]], baseJogo = Base {posicaoBase = (2,0)}, portaisJogo = [Portal {posicaoPortal = (0,0), ondasPortal = []}], torresJogo = []}
+  True
+  
+  TODO: Assim que todas as validações estiverem implementadas, verificar, alterar e adicionar mais exemplos.
+-}
 validaJogo :: Jogo -> Bool
-validaJogo Jogo { 
+validaJogo Jogo {
+              mapaJogo = _,
+              baseJogo = _,
+              portaisJogo = [] } = False
+validaJogo Jogo {
+              mapaJogo = [],
+              baseJogo = _,
+              portaisJogo = _ } = False
+validaJogo j@(Jogo { 
               mapaJogo = mapa,
-              baseJogo = Base { posicaoBase = (baseX,baseY)},
-              portaisJogo = [ Portal {posicaoPortal = (x,y)} ] } =
-                undefined
+              baseJogo = base,
+              portaisJogo = portais }) =
+                validaPortais portais mapa base && not (verificaSobreposicoes j)
 
--- A função que eu estava a falar era esta: deve funcionar mas não consegui testar 
-validaPosicaoObjeto :: (Float, Float) -> Jogo -> Bool
-validaPosicaoObjeto (x,y) (Jogo {mapaJogo = mapa}) = 
-  x >= 0 && x < fromIntegral (length (head mapa)) && y >= 0 && y < fromIntegral (length mapa)
+{-|
+  A função 'verificaSobreposicoes' verifica, para cada objetos presente num 'Jogo', se esse está sobreposto a outro objeto que não devia.
 
-{- existeBase :: Jogo ->  -}
+  ==__Exemplos de utilização__
+  >>> verificaSobreposicoes Jogo {baseJogo = Base {posicaoBase = (0,0)}, portaisJogo = [Portal {posicaoPortal = (1,0)}], torresJogo = [Torre {posicaoTorre = (2,0)}]}
+  False
+  >>> verificaSobreposicoes Jogo {baseJogo = Base {posicaoBase = (0,0)}, portaisJogo = [Portal {posicaoPortal = (0,0)}], torresJogo = [Torre {posicaoTorre = (2,0)}]}
+  True
+  >>> verificaSobreposicoes Jogo {baseJogo = Base {posicaoBase = (0,0)}, portaisJogo = [Portal {posicaoPortal = (2,0)}], torresJogo = [Torre {posicaoTorre = (2,0)}]}
+  True
+-}
+verificaSobreposicoes :: Jogo -> Bool
+verificaSobreposicoes (Jogo {baseJogo = base, portaisJogo = portais, torresJogo = torres}) = 
+    verificaCollisionPortais portais torres base
--- test/Tarefa1Spec.hs
+++ test/Tarefa1Spec.hs
@@ -2,11 +2,19 @@
 module Tarefa1Spec (testesTarefa1) where
 
 import Test.HUnit
+import Tarefa1 (validaJogo)
+import TData.TestData
 
 testesTarefa1 :: Test
 testesTarefa1 =
   TestLabel "Testes Tarefa 1" $
     test
-      [ "basic example test" ~: (2 :: Int) ~=? 1 + 1,
-        "another basic example" ~: True ~=? not False
+      [ 
+        "teste Jogo valido" ~: True ~=? validaJogo jogoValido,
+        "teste Jogo inválido sem portais" ~: False ~=? validaJogo jogoNoPortals,
+        "teste Jogo inválido sem mapa" ~: False ~=? validaJogo jogoNoMap,
+        "teste Jogo válido com mapa complexo" ~: True ~=? validaJogo jogoValidoMapTest,
+        "teste Jogo portal com pos inválida" ~: False ~=? validaJogo jogoValidoInvalidPortalPos
       ]
+
+
