f4bed48454aa383d2c02f647e2ce6010277a8757
RicoPleasure <esprazj.dev@gmail.com>
2024-12-05 23:16:09 +0000

    feat: implement tower range function

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -12,7 +12,11 @@ module Tarefa2 where
 import LI12425
 
 inimigosNoAlcance :: Torre -> [Inimigo] -> [Inimigo]
-inimigosNoAlcance = undefined
+inimigosNoAlcance (Torre {posicaoTorre = posicaoT, alcanceTorre = alcanceT}) inimigos = filter (\inimigo -> noAlcance posicaoT (posicaoInimigo inimigo) alcanceT) inimigos
+
+noAlcance :: Posicao -> Posicao -> Float -> Bool
+noAlcance (x1,y1) (x2,y2) alcance = distancia <= alcance
+    where distancia = sqrt((x2-x1)^2 + (y2-y1)^2)
 
 atingeInimigo :: Torre -> Inimigo -> Inimigo
 atingeInimigo = undefined
--- test/Tarefa2Spec.hs
+++ test/Tarefa2Spec.hs
@@ -1,11 +1,14 @@
 module Tarefa2Spec (testesTarefa2) where
 
 import Test.HUnit
+import LI12425
+import Tarefa2
 
 testesTarefa2 :: Test
 testesTarefa2 =
-  TestLabel "Testes Tarefa 2" $
-    test
-      [ "basic example test" ~: (2 :: Int) ~=? 1 + 1,
-        "another basic example" ~: True ~=? not False
-      ]
+  TestLabel "Testes Tarefa 2" $ test (testInimigosNoAlcance)
+
+torreTeste1 = Torre { posicaoTorre = (0, 0), alcanceTorre = 5.0 }
+inimigosTeste1 = [Inimigo { posicaoInimigo = (2, 4) }, Inimigo { posicaoInimigo = (10, 10) }]
+
+testInimigosNoAlcance = TestList [ TestCase (assertEqual "Teste 1"  [(Inimigo {posicaoInimigo = (10, 10)})] (inimigosNoAlcance torreTeste1 inimigosTeste1))]
